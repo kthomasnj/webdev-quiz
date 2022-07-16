76e19b2259f6ed24506c619a61adbb4edff7d566
Keith S Thomas <kthomasnj@hotmail.com>
2022-07-16 12:29:10 -0400

    Updated README.md; Added logic to get the Start Quiz button to work

---
+++ README.md
@@ -0,0 +1,50 @@
+# Web APIs Challenge
+
+## User Story
+---
+* AS A: coding boot camp student.
+* I WANT: to take a timed quiz on JavaScript fundamentals that stores high scores.
+* SO THAT: I can gauge my progress compared to my peers.
+
+
+## Acceptance Criteria
+---
+
+### GIVEN: I am taking a code quiz.
+
+* WHEN: I click the start button.
+* THEN: a timer starts and I am presented with a question.
+* WHEN: I answer a question.
+* THEN: I am presented with another question.
+* WHEN: I answer a question incorrectly.
+* THEN: time is subtracted from the clock.
+* WHEN: all questions are answered or the timer reaches 0.
+* THEN: the game is over.
+* WHEN: the game is over.
+* THEN: I can save my initials and score.
+
+## Mock-Up
+
+### Opening Page
+
+![Web APIs Challenge Mock-Up](./assets/images/web-dev-mock.png)
+<br>
+
+### Question Page
+![Web APIs Challenge Mock-Up](./assets/images/mock-question.png)
+<br>
+
+### Scores Page
+![Web APIs Challenge Mock-Up](./assets/images/mock-scores.png)
+<br>
+
+## Deployed Site
+---
+![Web APIs Challenge Live Site Image](./assets/images/web-dev-live.png)
+
+https://kthomasnj.github.io/webdev-quiz/
+<br>
+<br>
+<br>
+<hr>
+© 2022 Keith S. Thomas
---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var startQuizBtn = document.querySelector("#start-quiz");
+var pageOne = document.querySelector("#page-one");
+var pageTwo = document.querySelector("#page-two");
+var clock = document.querySelector("#timer");
+var questionsSpan = document.querySelector("#questions");
+var questions = ["Question 1", "Question 2", "Question 3", "Questioon 4"];
+var answerOptionsQ1 = ["Red", "Blue", "Green", "Yellow"];
+var answerOptionsQ2 = ["Black", "Brown", "Teal", "Gray"];
+var answerOptionsQ3 = ["White", "Light Grey", "Silver", "Gold"];
+var answerOptionsQ4 = ["Peach", "Orange", "Pink", "Aqua"];
+
+function startQuiz () {
+    console.log("It worked!!!");
+}
+
+
+
+
+
+
+// var startQuizBtn = document.querySelector("#start-quiz");
+// var pageOne = document.querySelector("#first-page");
+// var pageTwo = document.querySelector("#question");
+
+
+// var time = 60;
+
+// function startQuiz () {
+//     if(time > 0) {
+//         timeRemaining.innerHTML = "Time: " + time;
+//         time--;
+
+//         console.log(time);
+    
+//     } else if (time === 0) {
+//         timeRemaining.innerHTML = "TIMES UP";
+//     }
+// }
+
+
+
+
+//     startQuiz();
+// });
+
+startQuizBtn.addEventListener("click", function () {
+    pageOne.style.display = "none";
+    pageTwo.style.display = "flex";
+
+    startQuiz();
+})
