29bad0e00929ef41f3dd9b55c06c6459fb44adb8
Keith S Thomas <kthomasnj@hotmail.com>
2022-07-17 12:06:36 -0400

    Added feature to save scores to the scores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,13 @@ var pageTwo = document.querySelector("#page-two");
 var pageThree = document.querySelector("#page-three");
 var clock = document.querySelector("#timer");
 var currentScore = document.querySelector("#score-span");
+var questionsArray = ["Commonly used data types DO Not include:",
+    "The condition in an if/else statement is enclosed with ______.",
+    "Arrays in JavaScript can be used to store ________",
+    "String values must be enclosed within ______ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing content to the debugger is:"];
 var score = 0;
-var time = 2;
+var time = 1;
 
 function startTimer() {
     setInterval(function () {
@@ -20,9 +25,15 @@ function startTimer() {
             time--;
         } else {
             return finalScore();
-            
+
         }
     }, 1000);
+}
+
+function displayQuestion() {
+    for (i = 0; i > questionsArray.length; i++) {
+        console.log(questionsArray[i]);
+    }
 }
 
 function finalScore() {
@@ -33,5 +44,6 @@ startQuizBtn.addEventListener("click", function () {
     pageOne.style.display = "none";
     pageTwo.style.display = "flex";
 
+    displayQuestion();
     startTimer();
-});
+    });
